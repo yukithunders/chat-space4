5bfa802a4940c2c1dace62cf459aa2e7ae593015
yukithunders <yukiyoshikawa0209@gmail.com>
2019-09-11 17:01:47 +0900

    update_inc

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,12 +1,100 @@
 $(function(){
-  $(".chat-group-form__input").on("keyup",function(){
-    var input = $(".chat-group-form__input").val();
+  var name_list = $(".user-search-add-list");
+  var member_list = $(".user-search-result");
 
+  function appendUser(user){
+
+      var html = `<div class="chat-group-user clearfix">
+      <p class="chat-group-user__name">
+      ${user.name}
+      </p>
+      <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name}>追加</div>
+    </div>`
+
+    name_list.append(html);
+  }
+
+  function append_add_Member(user_id,user_name){
+
+    var add_list_html = `<div class="chat-group-user clearfix">
+    <input name='group[user_ids][]' type='hidden' value=${user_id}>
+  <p class='chat-group-user__name'>${user_name}</p>
+  <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn' data-user-id=${user_id} data-user-name=${user_name}>削除</div>`
+    member_list.append(add_list_html);
+
+    
+  }
+
+  function append_del_Member(user_id,user_name){
+
+    var html = `<div class="chat-group-user clearfix">
+      <p class="chat-group-user__name">
+      ${user_name}
+      </p>
+      <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user_id} data-user-name=${user_name}>追加</div>
+    </div>`
+
+    name_list.append(html);
+  }
+  $(document).on('click','.user-search-add',function(){
+    console.log("アイウエオ");
+    var user_id = $(this).attr('data-user-id');
+    console.log(user_id);
+    var user_name = $(this).attr('data-user-name');
+    console.log(user_name);
+    $(this).parent().remove();
+    append_add_Member(user_id,user_name);
+  })
+
+  $(document).on('click','.user-search-remove',function(){
+    console.log("かきくけこ");
+    var user_id = $(this).attr('data-user-id');
+    console.log(user_id);
+    var user_name = $(this).attr('data-user-name');
+    console.log(user_name);
+    $(this).parent().remove();
+    console.log($(this).parent());
+    append_del_Member(user_id,user_name);
+  })
+
+  $(".user-search-field").on("keyup", function() {
+    var input = $(".user-search-field").val();
+    console.log(input);
     $.ajax({
       type: 'GET',
-      url: '/products/search',
+      url:  '/users',
       data: {keyword: input},
-      dataType: 'json'
+      dataType: 'json',
+    })
+
+    .done(function(users){
+      console.log(users);
+      $('.user-search-add-list').empty();
+      if(users.length !== 0){
+        users.forEach(function(user){
+          appendUser(user);
+        });
+      }else{
+        //エラー
+      }
+    })
+    .fail(function() {
+      alert('検索に失敗しました');
+    });
+  });
+
+  $('#new_group').on('submit',function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    console.log(formData);
+    console.log("サシスセソ");
+    $.ajax({
+      type: 'POST',
+      url: '/groups',
+      data: formData, 
+      dataType: "json",
+      processData: false,
+      contentType: false
     })
   });
 });
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
 
   def index
-  
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").limit(20)
+    #binding.pry
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
   
   def edit
